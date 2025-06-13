111f50ac1cac7d6342717bcd8400281e80d7ec06
lua-luau <disposableacc252@gmail.com>
2025-06-13 12:41:27 +0700

    update hitbox.lua

--- luau/hitbox.lua
+++ luau/hitbox.lua
@@ -135,7 +135,7 @@ VisualTab:Toggle("Character Highlight", function(state)
     getgenv().fillcolor = Color3.new(0, 0, 0)
     getgenv().outlinecolor = Color3.new(1, 1, 1)
     getgenv().filltrans = 0.5
-    getgenv().outlinetrans = 0.5
+    getgenv().outlinetrans = 0.3
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/RobloxScripts/main/Highlight-ESP.lua"))()
 end)
 
