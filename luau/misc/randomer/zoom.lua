ccf59b52c60de3ae28c21e0ceeedde8b0e4add16
lua-luau <disposableacc252@gmail.com>
2025-10-12 10:31:53 +0700

    update zoom.lua

--- luau/misc/randomer/zoom.lua
+++ luau/misc/randomer/zoom.lua
@@ -8,20 +8,26 @@ local originalFOV = Camera.FieldOfView
 local zoomedFOV = originalFOV - 60
 local isZoomed = false
 
--- Keep FOV enforced every frame
-RunService.RenderStepped:Connect(function()
-	if isZoomed then
-		Camera.FieldOfView = math.max(1, zoomedFOV)
-	else
-		Camera.FieldOfView = originalFOV
-	end
-end)
-
--- Toggle zoom on J press
+-- Toggle zoom when J is pressed
 UserInputService.InputBegan:Connect(function(input, gameProcessed)
 	if gameProcessed then return end
 
 	if input.KeyCode == Enum.KeyCode.J then
 		isZoomed = not isZoomed
+
+		-- Update originalFOV when toggling back
+		if not isZoomed then
+			originalFOV = Camera.FieldOfView
+			zoomedFOV = originalFOV - 60
+		end
+	end
+end)
+
+-- Bind to render step with high priority so it overrides other scripts
+RunService:BindToRenderStep("EnforceFOV", Enum.RenderPriority.Last.Value + 1000, function()
+	if isZoomed then
+		Camera.FieldOfView = math.max(1, zoomedFOV)
+	else
+		Camera.FieldOfView = originalFOV
 	end
 end)
